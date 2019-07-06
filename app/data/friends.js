4ebaface731f24e9ea7226deedd24195159a7056
ArthurM0rgan <46456968+TonyPrpich@users.noreply.github.com>
2019-07-05 22:40:53 -0600

    added friends list

---
+++ app/data/friends.js
@@ -0,0 +1,101 @@
+var friends = [
+    {
+        name: "Mike",
+        photo: "http://a.espncdn.com/combiner/i?img=/i/headshots/mlb/players/full/30836.png&w=350&h=254",
+        scores: [
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+        ]
+    },
+    {
+        name: "Nancy",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/a/a5/Official_photo_of_Speaker_Nancy_Pelosi_in_2019.jpg",
+        scores: [
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+        ]
+    },
+    {
+        name: "Roger",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7f/062707_267_Roger_Clemens.jpg/440px-062707_267_Roger_Clemens.jpg",
+        scores: [
+            "3",
+            "2",
+            "1",
+            "3",
+            "2",
+            "1",
+            "3",
+            "2",
+            "1",
+            "5"
+        ]
+    },
+    {
+        name: "Sammy",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/2/29/Sosa_Spring_training.jpg",
+        scores: [
+            "3",
+            "2",
+            "1",
+            "3",
+            "2",
+            "1",
+            "3",
+            "2",
+            "1",
+            "4"
+        ]
+    },
+    {
+        name: "Hillary",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/Hillary_Clinton_official_Secretary_of_State_portrait_crop.jpg/440px-Hillary_Clinton_official_Secretary_of_State_portrait_crop.jpg",
+        scores: [
+            "5",
+            "4",
+            "3",
+            "2",
+            "1",
+            "5",
+            "4",
+            "3",
+            "2",
+            "1"
+        ]
+    },
+    {
+        name: "Tony",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4e/Tony_Romo_2015.jpg/400px-Tony_Romo_2015.jpg",
+        scores: [
+            "5",
+            "4",
+            "3",
+            "2",
+            "1",
+            "5",
+            "4",
+            "3",
+            "2",
+            "1"
+        ]
+    }
+]
+
+
+module.exports = friends;
