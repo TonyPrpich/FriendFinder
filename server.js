a6d36fe997094bb1916bf72f7ef8ccd624fd792d
ArthurM0rgan <46456968+TonyPrpich@users.noreply.github.com>
2019-07-18 19:23:39 -0600

    routes

--- server.js
+++ server.js
@@ -21,7 +21,8 @@ app.use(express.json());
 //     console.log("connected as id " + connection.threadId);
 // })
 
-
+require("./app/routing/apiRoutes")(app)
+require("./app/routing/htmlRoutes")(app)
 
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
